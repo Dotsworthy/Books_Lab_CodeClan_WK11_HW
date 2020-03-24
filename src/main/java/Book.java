945591e289d11c3ad7739b98ff1458344ee126ec
Dotsworthy <dotsworthy@gmail.com>
2020-03-24 20:59:21 +0000

    initial commit

---
+++ src/main/java/Book.java
@@ -0,0 +1,12 @@
+public class Book {
+    private String name;
+    private String author;
+    private String genre;
+
+    public Book(String name, String author, String genre) {
+        this.name = name;
+        this.name = author;
+        this.genre = genre;
+    }
+}
+
