121975c8e5e6cde1e030bbb4cde96d16507016df
Dotsworthy <dotsworthy@gmail.com>
2020-03-24 21:17:44 +0000

    added book count

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> book;
+
+    public Library() {
+        this.book = new ArrayList<Book>();
+    }
+
+    public int getBookCount() {
+       return this.book.size();
+    }
 
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LibraryTest {
+
+    Library library;
+    Book book;
+
+    @Before
+    public void before() {
+        library = new Library();
+        book = new Book("Vietnam War", "Max Hastings","History");
+    }
+
+    @Test
+    public void canGetBookCount() {
+        assertEquals(0, library.getBookCount());
+    }
+
+    
 }
